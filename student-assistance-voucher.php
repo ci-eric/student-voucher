7b4431c94edafcb01306d9efe6a33be526bd82fe
Eric 'Zander' Nelson <ci.eric.nelson@gmail.com>
2013-11-15 11:57:00 -0800

    layout creation

---
+++ student-assistance-voucher.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-cell-Type" content-cell="text/html; charset=utf-8" />
+<link type="text/css" rel="stylesheet" href="student.css" />
+<title>Student Voucher</title>
+</head>
+
+<body>
+
+    <?php include "student-voucher.php"; ?>
+
+    <div class="certify center bold print">I CERTIFY THAT THE HOURS REPORTED ABOVE ARE TRUE AND CORRECT.</div>
+
+	<div class="signature center print"><p>______________________________________________
+		____________________________________________ <br />
+		Student Employee Signature  Date Supervisor Signature Date</p></div>
+
+	<div class="signature center">
+		<table>
+			<tbody>
+				<tr>
+					<td>___________________________________</td>
+					<td>___________</td>
+					<td>___________________________________</td>
+					<td>___________</td>
+				</tr>
+				<tr>
+					<td>Student Employee Signature</td>
+					<td>Date</td>
+					<td>Supervisor Signature</td>
+					<td>Date</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+
+	<div class="payroll payroll-left bold print float-l">
+	*When school is in session, student assistants may work up to but not in excess of 20 hours per week<br />
+	*When school is NOT in session, student assistants may work up to a maximum of 40 hours per week<br />
+	 – but SHALL NOT be scheduled to work overtime.
+	</div>
+	<div class="payroll payroll-center print float-l">
+	</div>
+	<div class="payroll payroll-right bold print float-l">
+	    <div class="bg-lightGrey pad-left-25">FOR PAYROLL SERVICES USE ONLY</div>
+	    <div class="payroll-middle pad-left-25">Position #:</div>
+	    <div class="pad-left-25">Department:</div>
+	</div>
+
+</body>
+</html>
